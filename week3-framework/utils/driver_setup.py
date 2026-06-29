afd83883f1d8724a0fddc607596ad234010a1958
Herfando <herfando_91@yahoo.com>
2026-06-29 20:38:22 +0700

    new file:   .github/workflows/ci.yml         modified:   utils/driver_setup.py

--- week3-framework/utils/driver_setup.py
+++ week3-framework/utils/driver_setup.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 def create_driver():
     options = Options()
 
-    driver = webdriver.Chrome(options=options)
+    # 🔥 IMPORTANT: headless mode untuk CI
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
     return driver

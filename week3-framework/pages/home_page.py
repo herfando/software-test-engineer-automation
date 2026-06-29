d8fcc217fd1dd83b819e9e537be6475368e9052b
Herfando <herfando_91@yahoo.com>
2026-06-29 20:55:22 +0700

    modified:   pages/__pycache__/home_page.cpython-314.pyc         modified:   pages/home_page.py         modified:   tests/__pycache__/test_search_movie.cpython-314-pytest-9.1.1.pyc

--- week3-framework/pages/home_page.py
+++ week3-framework/pages/home_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from config.config import BASE_URL 
 
 class HomePage:
     def __init__(self, driver):
@@ -9,14 +10,11 @@ class HomePage:
         self.wait = WebDriverWait(driver, 10)
 
     def open(self):
-        self.driver.get("https://movie-app-next-xi.vercel.app/home")
+        self.driver.get(BASE_URL + "/home")
 
     def search_movie(self, keyword):
         search_box = self.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, "input[placeholder='Search Movie']")
-            )
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "input[placeholder='Search Movie']"))
         )
-
-        search_box.clear()
+        search_box.click()
         search_box.send_keys(keyword)
